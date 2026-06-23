0c552748810e10385df5e0b6bccac50f20f35ae3
jasmine nisha <nishajas291@gmail.com>
2026-06-24 01:16:56 +0530

    Delete logger.py

--- logger.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-def log_attack(user_input):
-    with open("attack_log.txt", "a") as file:
-        file.write(f"[{datetime.now()}] Suspicious Prompt: {user_input}\n")
